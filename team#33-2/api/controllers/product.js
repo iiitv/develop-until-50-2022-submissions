58422f331765a1aee7963593a4f89b6649288301
Gohil Deepak <B61309@mail.jiit.ac.in>
2022-10-09 16:18:40 +0530

    filters for searching,category,brand,price added to products

--- team#33-2/api/controllers/product.js
+++ team#33-2/api/controllers/product.js
@@ -30,8 +30,61 @@ const delete_product = async (req, res, next) => {
 
 const get_all_products = async (req, res, next) => {
     try {
-        const products = await Product.find().select("name price breif url")
-        res.send(products)
+        if (req.query.cat && req.query.limit) {
+            const products = await Product.find({ category: req.query.cat }).where("price").lte(parseInt(req.query.limit)).select("name price breif url")
+            res.send(products)
+        }
+        else if (req.query.brand && req.query.limit) {
+            const products = await Product.find({ brand: req.query.brand }).where("price").lte(parseInt(req.query.limit)).select("name price breif url")
+            res.send(products)
+        }
+        else if (req.query.cat) {
+            const products = await Product.find({ category: req.query.cat }).select("name price breif url")
+            res.send(products)
+        }
+        else if (req.query.brand) {
+            const products = await Product.find({ brand: req.query.brand }).select("name price breif url")
+            res.send(products)
+        }
+        else if (req.query.limit) {
+            const products = await Product.find().where("price").lte(parseInt(req.query.limit)).select("name price breif url")
+            res.send(products)
+        }
+        else {
+            const products = await Product.find().select("name price breif url")
+            res.send(products)
+        }
+    } catch (error) {
+        next()
+    }
+}
+
+const get_all_products_of_name = async (req, res, next) => {
+    try {
+        if (req.query.cat && req.query.limit) {
+            const products = await Product.find({ name: req.params.title, category: req.query.cat }).where("price").lte(parseInt(req.query.limit)).select("name price breif url")
+            res.send(products)
+        }
+        else if (req.query.brand && req.query.limit) {
+            const products = await Product.find({ name: req.params.title, brand: req.query.brand }).where("price").lte(parseInt(req.query.limit)).select("name price breif url")
+            res.send(products)
+        }
+        else if (req.query.cat) {
+            const products = await Product.find({ name: req.params.title, category: req.query.cat }).select("name price breif url")
+            res.send(products)
+        }
+        else if (req.query.brand) {
+            const products = await Product.find({ name: req.params.title, brand: req.query.brand }).select("name price breif url")
+            res.send(products)
+        }
+        else if (req.query.limit) {
+            const products = await Product.find({ name: req.params.title }).where("price").lte(parseInt(req.query.limit)).select("name price breif url")
+            res.send(products)
+        }
+        else {
+            const products = await Product.find({ name: req.params.title }).select("name price breif url")
+            res.send(products)
+        }
     } catch (error) {
         next()
     }
@@ -47,4 +100,4 @@ const get_product = async (req, res, next) => {
     }
 }
 
-module.exports = { add_product, update_product, delete_product, get_all_products, get_product }
+module.exports = { add_product, update_product, delete_product, get_all_products, get_all_products_of_name, get_product }
--- team#33-2/api/routes/product.js
+++ team#33-2/api/routes/product.js
@@ -1,5 +1,5 @@
 const express = require("express")
-const { add_product, update_product, delete_product, get_all_products, get_product } = require("../controllers/product")
+const { add_product, update_product, delete_product, get_all_products, get_all_products_of_name, get_product } = require("../controllers/product")
 
 const router = express.Router()
 
@@ -14,6 +14,7 @@ router.delete("/:id", delete_product)
 
 // GET ALL
 router.get("/", get_all_products)
+router.get("/name/:title", get_all_products_of_name)
 
 // GET
 router.get("/:id", get_product)
