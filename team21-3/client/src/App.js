7c3cf056c2632687663313382bcc9547b35a0d90
aradhya-10 <202051034@iiitvadodara.ac.in>
2022-10-09 12:34:14 +0530

    Add team#21

---
+++ team21-3/client/src/App.js
@@ -0,0 +1,13 @@
+// import logo from './logo.svg';
+import './App.css';
+// import Navabr from "./Components/Navbar"
+
+function App() {
+  return (
+    <>
+		hello world
+	</>
+  );
+}
+
+export default App;
