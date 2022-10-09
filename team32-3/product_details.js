0ba2927cb59242ba585ca07e50ba960b8e50d2fc
AniModi <98632268+AniModi@users.noreply.github.com>
2022-10-09 22:57:38 +0530

    Add files via upload

---
+++ team32-3/product_details.js
@@ -0,0 +1,39 @@
+var details = 
+{
+    "life_p1":{
+        mg1:"styles/src/lifestyle/product1/product1_1.png",
+        img2:"styles/src/lifestyle/product1/product1_2.png",
+        img3:"styles/src/lifestyle/product1/product1_3.png",
+        img4:"styles/src/lifestyle/product1/product1_4.png"
+    },
+    "life_p2":{
+        img1:"styles/src/lifestyle/product1/product2_1.png",
+        img2:"styles/src/lifestyle/product1/product2_2.png",
+        img3:"styles/src/lifestyle/product1/product2_3.png",
+        img4:"styles/src/lifestyle/product1/product2_4.png"
+    },
+    "life_p3":{
+        mg1:"styles/src/lifestyle/product1/product3_1.png",
+        img2:"styles/src/lifestyle/product1/product3_2.png",
+        img3:"styles/src/lifestyle/product1/product3_3.png",
+        img4:"styles/src/lifestyle/product1/product3_4.png"
+    },
+    "life_p4":{
+        mg1:"styles/src/lifestyle/product1/product4_1.png",
+        img2:"styles/src/lifestyle/product1/product4_2.png",
+        img3:"styles/src/lifestyle/product1/product4_3.png",
+        img4:"styles/src/lifestyle/product1/product4_4.png"
+    },
+    "stat_p1":{
+        img1:"styles/src/stationery/product1/product1_1.png",
+        img1:"styles/src/stationery/product1/product1_2.png",
+        img1:"styles/src/stationery/product1/product1_3.png",
+        img1:"styles/src/stationery/product1/product1_4.png"
+    },
+    "games_p1":{
+        img1:"styles/src/games/game1/game1_1.png",
+        img2:"styles/src/games/game1/game1_2.png",
+        img3:"styles/src/games/game1/game1_3.png",
+        img4:"styles/src/games/game1/game1_4.png",
+    }
+}
